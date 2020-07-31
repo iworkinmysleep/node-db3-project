655ca65685fdfd7316484545f5598c581488e000
Jason Hinton <jason_hinton@att.net>
2020-07-30 22:40:37 -0400

    almost mvp

---
+++ schemes/scheme-model.js
@@ -0,0 +1,36 @@
+const db = require("../data/db-config.js");
+
+module.exports = {
+	find,
+	findById,
+	findSteps,
+	add,
+	// update,
+	// remove,
+};
+
+function find() {
+	return db("schemes");
+}
+
+function findById(id) {
+	return db("schemes").where({ id }).first();
+}
+
+function findSteps(id) {
+	return db("schemes as sc")
+		.join("steps as st", "st.scheme_id", "sc.id")
+		.select("sc.id", "sc.scheme_name", "st.step_number", "st.instructions")
+		.where({ scheme_id: id })
+		.orderBy("st.step_number");
+}
+
+function add(scheme) {
+  return db('schemes').insert(scheme);
+}
+
+function update(changes, id) {
+  
+}
+
+function remove() {}
